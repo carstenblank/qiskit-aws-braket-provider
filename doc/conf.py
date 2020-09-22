fe85e14f42cecf17f21c5829f10ec33ecf3e6c6f
Carsten Blank <blank@data-cybernetics.com>
2020-09-22 14:00:39 +0200

    Fix one error.

--- doc/conf.py
+++ doc/conf.py
@@ -56,4 +56,5 @@ html_theme = 'alabaster'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+master_doc = "index"
 apidoc_module_dir = "../src/qiskit_aws_braket_provider"
